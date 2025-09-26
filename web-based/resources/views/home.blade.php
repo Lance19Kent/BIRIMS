db67fa0f3be77972712bebef2feeeea9bca1653d
Lance Kent <lancemagollado46@gmail.com>
2025-09-26 15:20:17 +0800

    Trying CSS

--- web-based/resources/views/home.blade.php
+++ web-based/resources/views/home.blade.php
@@ -4,8 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home Page</title>
+    @vite('resources/css/app.css')
+    @vite('resources/js/app.js')
 </head>
 <body>
     <h1>Hello World</h1>
 </body>
-</html>
+</html> 
