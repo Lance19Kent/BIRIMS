2903c5cc0396ba9a673d84fe5b2f2915c5318010
Lance Kent <lancemagollado46@gmail.com>
2025-09-27 11:17:27 +0800

    fixed spacing problem

--- web-based/resources/views/home.blade.php
+++ web-based/resources/views/home.blade.php
@@ -4,17 +4,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>BIRIMS</title>
+    <link rel="icon" href="{{asset('img/Ellipse.png')}}" type="image/x-icon">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <link href="/web-based/resources/css/app.css" rel="stylesheet">
     @vite('resources/css/app.css')
     @vite('resources/js/app.js')
 </head>
 <body class="h-screen w-screen bg-[#F4F4F5]">
-    <header class="h-20 bg-white flex">
+    <header class="h-20 bg-white flex ml-10">
         <div class="h-[100%] w-[189px] flex items-center gap-2.5">
             <img src="{{asset('img/Ellipse.png')}}" alt="Barangay Logo">
-            <h1 class="font-medium font text-[30px] text-[#EA580C]  mt-[100px]">BIRIMS</h1>
+            <h1 class="font-medium font text-[30px] text-[#EA580C]">BIRIMS</h1>
         </div>
         <div class="h-[100%] w-full bg-sky-100">
             <ul>
