57433ec47a37fa80b69e320fbc417a4b120916fa
Lance Kent <lancemagollado46@gmail.com>
2025-09-28 17:58:27 +0800

    Second section done

--- web-based/resources/views/home.blade.php
+++ web-based/resources/views/home.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" class="scroll-pt-20 scroll-smooth">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -35,7 +35,7 @@
         </div>
     </header>
     <main class="min-h-screen w-full bg-[#F4F4F5] flex-1 flex flex-col">
-        <section class="h-screen w-full flex justify-between items-center-safe px-30">
+        <section class="h-screen w-full flex justify-between items-center-safe px-30 pt-[60px]">
             <div class="w-[550px] flex flex-col gap-[20px]">
                 <h1 class="text-[50px] font-medium leading-[65px]" style="font-family: 'IBM Plex Serif', serif;">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h1>
                 <p class="text-[18px] font-medium text-[#A1A1AA] leading-[27px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut  labore et dolore magna aliqua. Ut enim ad minim veniam, quis </p>
@@ -48,14 +48,41 @@
                 <img src="{{asset('img/hero-pic.png')}}" alt="Hero Image" class="w-[500px] h-[430px] whitespace-nowrap">
             </div>
         </section>
-        <section class="h-screen w-full bg-white" id="info-screen">
-            <div>
-            <h1 style="font-family: 'IBM Plex Serif', serif;" class="text-[50px] font-medium">Get Your Documents In Just 3 Easy Steps!</h1>
+        <section class="min-h-screen w-full bg-white flex flex-col items-center justify-center gap-[30px] pb-[100px]" id="info-screen">
+            <div class="flex flex-col gap-2 justify-center items-center">
+                <h1 style="font-family: 'IBM Plex Serif', serif;" class="text-[50px] font-medium">Get Your Documents In Just 3 Easy Steps!</h1>
                 <p class="font-medium text-[18px] text-[#A1A1AA]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut  labore et dolore </p>
+            </div>
+            <div class="w-full px-35 flex justify-between">
+                <div class="flex flex-col justify-center items-center">
+                    <img src="{{asset('img/fill-up.png')}}" alt="Step 1" class="w-[200px] h-[200px]">
+                    <h2 class="font-semibold text-[16px] mt-4 mb-2">Fill Up The Form</h2>
+                    <p class="font-medium text-[16px] text-[#A1A1AA] w-[200px] text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do</p>
+                </div>
+                <div class="flex flex-col justify-center items-center">
+                    <svg width="193" height="4" viewBox="0 0 193 4" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <line x1="0.994792" y1="2.48402" x2="192.995" y2="1.48402" stroke="#EA580C" stroke-width="2" stroke-dasharray="2 2"/>
+                    </svg>
+                </div>
+                <div class="flex flex-col justify-center items-center">
+                    <img src="{{asset('img/payment.png')}}" alt="Step 1" class="w-[200px] h-[200px]">
+                    <h2 class="font-semibold text-[16px] mt-4 mb-2">Payment</h2>
+                    <p class="font-medium text-[16px] text-[#A1A1AA] w-[200px] text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do</p>
+                </div>
+                <div class="flex flex-col justify-center items-center">
+                    <svg width="193" height="4" viewBox="0 0 193 4" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <line x1="0.994792" y1="2.48402" x2="192.995" y2="1.48402" stroke="#EA580C" stroke-width="2" stroke-dasharray="2 2"/>
+                    </svg>
+                </div>
+                <div class="flex flex-col justify-center items-center">
+                    <img src="{{asset('img/delivery.png')}}" alt="Step 1" class="w-[200px] h-[200px]">
+                    <h2 class="font-semibold text-[16px] mt-4 mb-2">Delivery</h2>
+                    <p class="font-medium text-[16px] text-[#A1A1AA] w-[200px] text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do</p>
+                </div>
             </div>
         </section>
     </main>
-    <footer class="h-[230px] bg-white flex justify-between items-center px-20">
+    <footer class="h-[230px] bg-amber flex justify-between items-center px-20">
         <div class="w-[450px]">
             <div class="flex items-center gap-2.5 mb-4">
                 <svg width="50" height="50" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
