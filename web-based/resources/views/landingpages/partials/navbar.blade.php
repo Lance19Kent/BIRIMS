69fdc68cee2f16f7f2daa429986647f9021b3343
Lance Kent <lancemagollado46@gmail.com>
2025-10-02 09:59:32 +0800

    Hover in header

--- web-based/resources/views/landingpages/partials/navbar.blade.php
+++ web-based/resources/views/landingpages/partials/navbar.blade.php
@@ -6,12 +6,12 @@
         <h1 class="font-medium font text-[30px] text-[#EA580C]">BIRIMS</h1>
     </div>
     <div class="h-[100%] w-full flex justify-end items-center pr-10">
-        <ul class="flex gap-[40px] text-[#A1A1AA]">
-            <li><a href="#">Home</a></li>
-            <li><a href="#step-section">Clearance</a></li>
-            <li><a href="#cards-certificates">Residency</a></li>
-            <li><a href="#blotter">Indigency</a></li>
-            <li><a href="#contact-us">Business</a></li>
+        <ul class="flex gap-[40px]">
+            <li><a href="#" class="header-nav">Home</a></li>
+            <li><a href="#step-section" class="header-nav">Clearance</a></li>
+            <li><a href="#cards-certificates" class="header-nav">Residency</a></li>
+            <li><a href="#blotter" class="header-nav">Indigency</a></li>
+            <li><a href="#contact-us" class="header-nav">Business</a></li>
             <li><a href="#create-account" class="p-[12px] bg-[var(--orange)] text-white rounded-[4px] border border-white duration-150 ease-in hover:bg-white hover:text-[var(--orange)] hover:border-[var(--orange)]">SIGN-IN</a></li>
         </ul>
     </div>
