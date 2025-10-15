fd91f61794decc0a10d3a57b32cc3e28c889c371
Lance Kent <lancemagollado46@gmail.com>
2025-10-15 22:03:21 +0800

    Added Footer again

--- web-based/resources/views/landingpages/partials/footer.blade.php
+++ web-based/resources/views/landingpages/partials/footer.blade.php
@@ -7,27 +7,28 @@
             <h1 class="font-medium font text-[30px] text-[#EA580C]">BIRIMS</h1>
         </div>
         <div class="flex flex-col gap-[10px]">
-            <a href="#" class="flex stroke-[var(--darkgray)] items-center text-[#A1A1AA] duration-150 ease-in gap-[10px] hover:text-[var(--orange)] hover:underline hover:stroke-[var(--orange)]">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" class="size-5">
+            <div class="flex stroke-[var(--darkgray)] items-center gap-[10px]">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" class="size-6">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
                     <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
                 </svg>
-                <p class="font-medium text-[12px]">P22M+7WC, Quirino Hwy, Novaliches, Quezon City, Metro Manila, Philippines</p>
-
-            </a>
-            <a href="#" class="flex stroke-[var(--darkgray)] items-center text-[#A1A1AA] duration-150 ease-in gap-[10px] hover:text-[var(--orange)] hover:underline hover:stroke-[var(--orange)]">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"  class="size-5">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z" />
+                <p class="font-medium text-[#A1A1AA] text-[12px]">P22M+7WC, Quirino Hwy, Novaliches, Quezon City, Metro Manila, Philippines</p>
+            </div>
+            <div class="flex stroke-[var(--darkgray)] items-center gap-[10px]">
+                <svg fill="var(--darkgray)" class="size-5" viewBox="0 0 1920 1920" xmlns="http://www.w3.org/2000/svg">
+                    <path d="m1416.013 791.915-30.91 225.617h-371.252v789.66H788.234v-789.66H449.808V791.915h338.426V585.137c0-286.871 176.207-472.329 449.09-472.329 116.87 0 189.744 6.205 231.822 11.845l-3.272 213.66-173.5.338c-4.737-.451-117.771-9.25-199.332 65.655-52.568 48.169-79.191 117.433-79.191 205.65v181.96h402.162Zm-247.276-304.018c44.446-41.401 113.71-36.889 118.787-36.663l289.467-.113 6.204-417.504-43.544-10.717C1511.675 16.02 1426.053 0 1237.324 0 901.268 0 675.425 235.206 675.425 585.137v93.97H337v451.234h338.425V1920h451.234v-789.66h356.7l61.932-451.233H1126.66v-69.152c0-54.937 14.214-96 42.078-122.058Z" fill-rule="evenodd"/>
                 </svg>
-                <p class="font-medium text-[12px]">282776775</p>
-            </a>
-            <a href="#" class="flex stroke-[var(--darkgray)] items-center text-[#A1A1AA] duration-150 ease-in gap-[10px] hover:text-[var(--orange)] hover:underline hover:stroke-[var(--orange)]">
+                <p class="font-medium text-[#A1A1AA] text-[12px]">www.facebook.com/brgysanbartolome2014</p>
+            </div>
+            <div class="flex stroke-[var(--darkgray)] items-center gap-[10px]">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" class="size-5">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75" />
                 </svg>
-            <p class="font-medium  text-[12px]">birim.sanbartolome@gmail.com</p>
-            </a>
+                
+                <p class="font-medium text-[#A1A1AA] text-[12px]">birim.sanbartolome@gmail.com</p>
+            </div>
         </div>
+
         <p class="font-medium text-[#A1A1AA] text-[12px]"></p>
 
     </div>
