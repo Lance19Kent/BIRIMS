6d3f0400a2a20a8d832c3a9894e6ec31a6ae56ac
Lance Kent <lancemagollado46@gmail.com>
2025-11-01 20:45:59 +0800

    Index Responsiveness Started

--- web-based/resources/views/index.blade.php
+++ web-based/resources/views/index.blade.php
@@ -25,33 +25,33 @@
     @include('partials.navbar')
     <!-- Navbar Section -->
 
-    <main class="min-h-screen max-w-full bg-[#F4F4F5] flex-1 flex flex-col">
+    <main class="min-h-screen w-full bg-[#F4F4F5] flex-1 flex flex-col">
 
         <!-- Hero Section -->
-        <section class="h-screen w-full flex md:flex-row justify-between items-center-safe px-30 pt-[60px] flex-col">
-            <div class="w-[550px] flex flex-col gap-[20px]">
-                <h1 class="md:text-[50px] font-medium leading-[65px] text-[32px]" style="font-family: 'IBM Plex Serif', serif;">Barangay Integrated Record and Incident Management System</h1>
-                <p class="text-[18px] font-medium text-[#A1A1AA] leading-[27px]">A digital platform designed to simplify and secure the management of barangay resident records, incidents, clearances, and certifications. </p>
+        <section class="min-h-screen w-full flex md:flex-row justify-between items-center md:px-30 px-[40px] md:pt-[60px] flex-col pt-[90px] text-center md:text-left md:gap-0 gap-[30px] pb-[60px] md:pb-0">
+            <div class="w-full md:max-w-[550px] flex flex-col gap-[20px] items-center md:items-start">
+                <h1 class="md:text-[50px] font-medium md:leading-[65px] text-[32px]" style="font-family: 'IBM Plex Serif', serif;">Barangay Integrated Record and Incident Management System</h1>
+                <p class="md:text-[18px] text-[16px] font-medium text-[#A1A1AA] leading-[27px]">A digital platform designed to simplify and secure the management of barangay resident records, incidents, clearances, and certifications. </p>
                 <a href="#step-section" class="w-[190px] flex items-center pt-[10px] pb-[10px] pl-[20px] pr-[20px] text-white border border-[var(--gray)] fill-white bg-[#EA580C] rounded-[4px] justify-center gap-2 duration-150 ease-in hover:bg-[var(--gray)] hover:text-[#EA580C] hover:fill-[#EA580C] hover:border-[var(--orange)]">
-                    <p class="text-[20px] font-medium">Get Started</p>
+                    <p class="md:text-[20px] font-medium">Get Started</p>
                     <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px">
                         <path d="M647-440H160v-80h487L423-744l57-56 320 320-320 320-57-56 224-224Z"/>
                     </svg>
                 </a>
             </div>
-            <div class="w-[550px] h-[430px] flex justify-end">
-                <img src="{{asset('assets/svg/hero-svg.svg')}}" alt="Hero Image" class="w-[500px] h-[430px] whitespace-nowrap">
+            <div class="w-full md:max-w-[550px] max-w-[220px] flex justify-end">
+                <img src="{{asset('assets/svg/hero-svg.svg')}}" alt="Hero Image">
             </div>
         </section>
         <!-- Hero Section -->
 
         <!-- Steps Section -->
-        <section class="h-screen w-full bg-white flex flex-col items-center justify-center gap-[30px] pb-[30px]" id="step-section">
+        <section class="min-h-screen w-full bg-white flex flex-col items-center justify-center gap-[30px] pb-[30px] px-[40px] md:px-0 text-center" id="step-section">
             <div class="flex flex-col gap-2 justify-center items-center">
-                <h1 style="font-family: 'IBM Plex Serif', serif;" class="text-[50px] font-medium">Get Your Documents In Just 3 Easy Steps!</h1>
-                <p class="font-medium text-[18px] text-[#A1A1AA]">Access barangay clearances, residency, indigency, and business permits online - fast, convenient, and secure.</p>
+                <h1 style="font-family: 'IBM Plex Serif', serif;" class="md:text-[50px] text-[32px] font-medium">Get Your Documents In Just 3 Easy Steps!</h1>
+                <p class="font-medium md:text-[18px] text-[16px] text-[#A1A1AA]">Access barangay clearances, residency, indigency, and business permits online - fast, convenient, and secure.</p>
             </div>
-            <div class="w-full px-35 flex justify-between">
+            <div class="w-full px-35 flex md:flex-row flex-col justify-between">
                 <div class="flex flex-col justify-center items-center">
                     <img src="{{asset('assets/svg/fill-up.svg')}}" alt="Fill-up Form Photo" class="w-[200px] h-[200px]">
                     <h2 class="font-semibold text-[16px] mt-4 mb-2">Fill Up The Form</h2>
@@ -82,7 +82,7 @@
         <!-- Steps Section -->
 
         <!-- Barangay Certificates Section -->
-        <section class="min-h-screen w-full flex flex-col items-center justify-center gap-[50px] py-8 pb-[80px] pt-[50px]" id="cards-certificates">
+        <!-- <section class="min-h-screen w-full flex flex-col items-center justify-center gap-[50px] py-8 pb-[80px] pt-[50px]" id="cards-certificates">
             <div class="flex flex-col gap-2 items-center justify-center">
                 <h1 style="font-family: 'IBM Plex Serif', serif;" class="font-medium text-[50px]">Request Your Barangay Certificates Online</h1>
                 <p class="font-medium text-[16px] text-[#A1A1AA] pl-[200px] pr-[200px] text-center">Easily apply for your barangay documents - no more long lines! Choose the certificate you need and submit your request in minutes.</p>
@@ -142,11 +142,11 @@
                     </div>
             </div>
             </div>
-        </section>
+        </section> -->
         <!-- Barangay Certificates Section -->
 
         <!-- Blotter Section -->
-        <section class="h-screen w-full bg-white flex items-center justify-center px-[150px] pb-[60px]" id="blotter">
+        <!-- <section class="h-screen w-full bg-white flex items-center justify-center px-[150px] pb-[60px]" id="blotter">
             <img src="{{asset('assets/svg/blotter-svg.svg')}}" alt="Blotter">
             <div class="flex flex-col gap-6 pl-20 w-[550px]">
                 <h1 style="font-family: var(--font-ibm-plex-serif);" class="font-medium text-[50px] leading-[65px]">Need To File For a Blotter</h1>
@@ -158,11 +158,11 @@
                     </svg>
                 </a>
             </div>
-        </section>
+        </section> -->
         <!-- Blotter Section -->
 
         <!-- FAQ Section -->  
-        <section class="h-screen w-full flex items-center justify-center pb-[80px]" id="contact-us">
+        <!-- <section class="h-screen w-full flex items-center justify-center pb-[80px]" id="contact-us">
             <div class="w-[600px] h-full flex flex-col gap-6 justify-center items-start px-20">
                 <h1 class="font-medium text-[50px] leading-[65px]" style="font-family: var(--font-ibm-plex-serif);">Frequently Asked Questions</h1>
                 <p class="font-medium text-[18px] text-[#A1A1AA]">If you have any question or concern, you can contact us in <a href="" class="text-[var(--orange)] duration-150 ease-in hover:underline">birims.sanbartolome@gmail.com</a> and we will quickly response at you! You can also contact us in different platforms just by clicking Contact Us below.</p>
@@ -217,11 +217,11 @@
                     <hr class="text-[#E4E4E7]">
                 </ul>
             </div>
-        </section>
+        </section> -->
         <!-- FAQ Section -->
 
         <!-- Create Account Section -->
-        <section class="w-full bg-white relative" style="height: calc(100vh - 80px);" id="create-account">
+        <!-- <section class="w-full bg-white relative" style="height: calc(100vh - 80px);" id="create-account">
             <div class="w-[155px] h-[234px] bg-[#EA580C] absolute rounded-br-[500px]"></div>
             <div class="w-[30px] h-[30px] bg-[#FFEDD5] rounded-full translate-x-[200px] translate-y-[50px] absolute"></div>
             <div class="w-[30px] h-[30px] bg-[#FB923C] rounded-full translate-x-[170px] translate-y-[100px] absolute"></div>
@@ -238,7 +238,7 @@
                 </div>
             </div>
             <img src="assets/svg/create-account.svg" class="absolute bottom-0 right-0 w-[200px]">
-        </section>
+        </section> -->
         <!-- Create Account Section -->
          
     </main>
