93262565ff16c28c3a9b032c29248bb2b7512c1e
Lance Kent <lancemagollado46@gmail.com>
2025-11-02 16:04:14 +0800

    signup page done

--- web-based/resources/views/signup.blade.php
+++ web-based/resources/views/signup.blade.php
@@ -167,12 +167,12 @@
                             <path stroke-linecap="round" stroke-linejoin="round" d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88" />
                     </svg>
                 </span>
-                <div class="flex gap-[8px]">
+                <div class="flex gap-[8px] jus">
                     <input type="checkbox" class="cursor-pointer accent-[var(--orange)]"><label class="md:text-[13px] text-[12px] font-semibold">I agree to the <a href="#" class="text-[var(--orange)] duration-150 ease-in hover:underline ">Terms and Conditions</a> and <a href="#" class="text-[var(--orange)] duration-150 ease-in hover:underline">Privacy Policy</a></label>
                 </div>
                 <div class="flex flex-col gap-4">
-                        <button type="button" class="bg-[var(--orange)] w-full border border-[var(--orange)] rounded-[4px] text-white text-[18px] text-[16px] text-center py-[8px] ease-out duration-150 hover:bg-white hover:text-[var(--orange)] cursor-pointer" id="backBtn">Previous</button>
-                        <a href="{{url('login')}}" class="bg-white block text-center w-full py-[8px] font-medium text-[var(--orange)] text-[18px] rounded-[4px] border border-[var(--orange)] duration-150 ease-out hover:bg-[var(--orange)] hover:text-white">Sign In</a>
+                        <button type="button" class="bg-[var(--orange)] w-full border border-[var(--orange)] rounded-[4px] text-white md:text-[18px] text-[16px] text-center py-[8px] ease-out duration-150 hover:bg-white hover:text-[var(--orange)] cursor-pointer" id="backBtn">Previous</button>
+                        <a href="{{url('login')}}" class="bg-white block text-center w-full py-[8px] font-medium text-[var(--orange)] md:text-[18px] text-[16px] rounded-[4px] border border-[var(--orange)] duration-150 ease-out hover:bg-[var(--orange)] hover:text-white">Sign In</a>
                 </div>  
             </div>
             </div>
