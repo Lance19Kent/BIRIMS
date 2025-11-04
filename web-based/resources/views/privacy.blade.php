1495d3a39be7e9160e3fef190f262769a31abbcc
Lance Kent <lancemagollado46@gmail.com>
2025-11-04 16:17:55 +0800

    Privacy Policy Responsive Done

--- web-based/resources/views/privacy.blade.php
+++ web-based/resources/views/privacy.blade.php
@@ -23,8 +23,8 @@
     @include('partials.navbar')
     <!-- Navbar Section -->
 
-    <main class="w-full min-h-screen flex-1 bg-[var(--gray)] relative md:pb-[40px] pb-[120px]">
-        <svg viewBox="0 0 185 185" fill="none" xmlns="http://www.w3.org/2000/svg" class="absolute bottom-0 right-0 md:w-[250px] w-[130px]">
+    <main class="w-full min-h-screen flex-1 bg-[var(--gray)] relative md:pb-[260px] pb-[120px]">
+        <svg viewBox="0 0 185 185" fill="none" xmlns="http://www.w3.org/2000/svg" class="absolute bottom-0 right-0 md:w-[250px] w-[130px] z-0">
         <g clip-path="url(#clip0_58_1146)">
             <path d="M185 65.366H65.3661V185H185V65.366Z" fill="#FDBA74"/>
             <path d="M125.184 5.55005H5.55005V125.184H125.184V5.55005Z" stroke="#111111" stroke-width="6" stroke-linecap="round" stroke-linejoin="round"/>
@@ -40,26 +40,27 @@
             <path d="M213.161 100.775C225.123 100.775 234.821 91.136 234.821 79.2458C234.821 67.3555 225.123 57.7166 213.161 57.7166C201.199 57.7166 191.501 67.3555 191.501 79.2458C191.501 91.136 201.199 100.775 213.161 100.775Z" fill="#FFEDD5"/>
             <path d="M213.161 207.781C225.123 207.781 234.821 198.142 234.821 186.252C234.821 174.362 225.123 164.723 213.161 164.723C201.199 164.723 191.501 174.362 191.501 186.252C191.501 198.142 201.199 207.781 213.161 207.781Z" fill="#FB923C"/>
         </svg>
-        <div class="flex flex-col items-center pt-[35px] gap-[10px]">
+        <div class=" flex flex-col items-center pt-[35px] gap-[10px]">
             <h1 style="font-family: 'IBM Plex Serif';" class="pt-[80px] relative z-10 md:text-[50px] text-[32px] font-medium">Privacy and Policy</h1>
             <p class="relative z-10 text-[var(--darkgray)] md:text-[16px] text-[14px]">Last Updated: October 24 2025</p>
         </div>
-        <div class="mt-[50px] flex flex-col md:flex-row justify-between gap-[50px] md:pr-[230px] md:pl-[250px]">
-           <div class="w-full flex justify-center">
-             <div class="w-full md:max-w-[410px] max-w-[300px] h-fit border border-[#71717A] flex flex-col rounded-[4px] sticky top-[150px]">
-                <h1 style="font-family: 'IBM Plex Serif';" class="font-medium text-[16px] md:text-[18px] p-[10px]">On this page</h1>
-                <a href="#introduction" class="font-medium text-[16px] md:text-[18px] text-[var(--darkgray)] py-[10px] pl-[10px] duration-150 ease-out hover:bg-[var(--iconorange)] hover:text-[var(--gray100)] privacy-nav-links active-privacy-nav">1. Introduction</a>
-                <a href="#information-we-collect" class="privacy-nav-links font-medium text-[16px] md:text-[18px] text-[var(--darkgray)] py-[10px] pl-[10px]  duration-150 ease-out hover:bg-[var(--iconorange)] hover:text-[var(--gray100)]">2. Information We Collect</a>
-                <a href="#use-of-services" class="privacy-nav-links font-medium text-[16px] md:text-[18px] text-[var(--darkgray)] py-[10px] pl-[10px] duration-150 ease-out hover:bg-[var(--iconorange)] hover:text-[var(--gray100)]">3. Use of Services</a>
-                <a href="#data-privacy-and-protection" class="privacy-nav-links font-medium text-[16px] md:text-[18px] text-[var(--darkgray)] py-[10px] pl-[10px] duration-150 ease-out hover:bg-[var(--iconorange)] hover:text-[var(--gray100)]">4. Data Privacy and Protection</a>
-                <a href="#data-sharing-and-disclosure" class="privacy-nav-links font-medium text-[16px] md:text-[18px] text-[var(--darkgray)] py-[10px] pl-[10px] duration-150 ease-out hover:bg-[var(--iconorange)] hover:text-[var(--gray100)]">5. Data Sharing and Disclosure</a>
-                <a href="#user-rights" class="privacy-nav-links font-medium text-[16px] md:text-[18px] text-[var(--darkgray)] py-[10px] pl-[10px] duration-150 ease-out hover:bg-[var(--iconorange)] hover:text-[var(--gray100)]">6. User Rights</a>
-                <a href="#updates-to-this-policy" class="privacy-nav-links font-medium text-[16px] md:text-[18px] text-[var(--darkgray)] py-[10px] pl-[10px] duration-150 ease-out hover:bg-[var(--iconorange)] hover:text-[var(--gray100)]">7. Updates to This Policy</a>
-                <a href="#contact-us" class="privacy-nav-links font-medium text-[16px] md:text-[18px] text-[var(--darkgray)] py-[10px] pl-[10px] duration-150 ease-out hover:bg-[var(--iconorange)] hover:text-[var(--gray100)]">8. Contact Us</a>
+        <div class="w-full mt-[50px] relative z-10 flex flex-col justify-center md:flex-row gap-[50px] px-[40px]">
+           <div class="md:w-[350px] sm-w-full flex ">
+             <div class="w-full md:max-w-[410px] bg-[var(--gray)] h-fit border border-[#71717A] flex flex-col rounded-[4px] sticky top-[150px]">
+                <h1 style="font-family: 'IBM Plex Serif';" class="font-medium text-[16px] md:text-[18px] p-[10px] text-center md:text-left ">On this page</h1>
+                <a href="#introduction" class="font-medium text-center md:text-left text-[16px] md:text-[18px] text-[var(--darkgray)] py-[10px] pl-[10px] duration-150 ease-out hover:bg-[var(--iconorange)] hover:text-[var(--gray100)] privacy-nav-links active-privacy-nav">1. Introduction</a>
+                <a href="#information-we-collect" class="privacy-nav-links font-medium text-[16px] md:text-[18px] text-[var(--darkgray)] py-[10px] pl-[10px]  duration-150 ease-out hover:bg-[var(--iconorange)] hover:text-[var(--gray100)] text-center md:text-left ">2. Information We Collect</a>
+                <a href="#use-of-services" class="privacy-nav-links font-medium text-[16px] md:text-[18px] text-[var(--darkgray)] py-[10px] pl-[10px] duration-150 ease-out hover:bg-[var(--iconorange)] hover:text-[var(--gray100)] text-center md:text-left ">3. Use of Services</a>
+                <a href="#data-privacy-and-protection" class="privacy-nav-links font-medium text-[16px] md:text-[18px] text-[var(--darkgray)] py-[10px] pl-[10px] duration-150 ease-out hover:bg-[var(--iconorange)] hover:text-[var(--gray100)] text-center md:text-left ">4. Data Privacy and Protection</a>
+                <a href="#data-sharing-and-disclosure" class="privacy-nav-links font-medium text-[16px] md:text-[18px] text-[var(--darkgray)] py-[10px] pl-[10px] duration-150 ease-out hover:bg-[var(--iconorange)] hover:text-[var(--gray100)] text-center md:text-left ">5. Data Sharing and Disclosure</a>
+                <a href="#user-rights" class="privacy-nav-links font-medium text-[16px] md:text-[18px] text-[var(--darkgray)] py-[10px] pl-[10px] duration-150 ease-out hover:bg-[var(--iconorange)] hover:text-[var(--gray100)] text-center md:text-left ">6. User Rights</a>
+                <a href="#updates-to-this-policy" class="privacy-nav-links font-medium text-[16px] md:text-[18px] text-[var(--darkgray)] py-[10px] pl-[10px] duration-150 ease-out hover:bg-[var(--iconorange)] hover:text-[var(--gray100)] text-center md:text-left ">7. Updates to This Policy</a>
+                <a href="#contact-us" class="privacy-nav-links font-medium text-[16px] md:text-[18px] text-[var(--darkgray)] py-[10px] pl-[10px] duration-150 ease-out hover:bg-[var(--iconorange)] hover:text-[var(--gray100)] text-center md:text-left ">8. Contact Us</a>
 
             </div>
            </div>
-            <div class="w-full max-w-[600px] h-fit flex flex-col gap-[10px] px-[40px] md:px-0">
+           <div class="w-full lg:w-fit flex justify-center">
+            <div class="w-full lg:max-w-[400px] h-fit flex flex-col gap-[10px] px-[20px] md:px-0">
                 <div id="introduction" class="section">
                     <h6 class="font-medium md:text-[18px] text-[16px]">1. Introduction</h6>
                     <p class="font-medium md:text-[18px] text-[16px] text-[var(--darkgray)] px-[15px] leading-8 mt-[6px]">At BIRIMS (Barangay Integrated Record and Incident Management System), we value your privacy and are committed to protecting the personal information you share with us.
@@ -116,7 +117,7 @@
                     <h6 class="font-medium md:text-[18px] text-[16px]">7. Updates to This Policy</h6>
                     <p class="font-medium md:text-[18px] text-[16px] text-[var(--darkgray)] px-[15px] leading-8 mt-[6px]">We may update this Privacy Policy from time to time. All changes will be reflected on this page with the updated date above.</p>
                 </div>
-                <div id="contact-us" class="section">
+                <div id="contact-us" class="section ">
                     <h6 class="font-medium md:text-[18px] text-[16px]">8. Contact Us</h6>
                     <p class="font-medium md:text-[18px] text-[16px] text-[var(--darkgray)] px-[15px] leading-8 mt-[6px]">If you have questions or concerns about this Privacy Policy, you may contact:
                         <a href="https://maps.app.goo.gl/t9oBHUwfVVHSXJFs9" target="_blank" class="flex gap-[10px] text-[16px] text-[var(--darkgray)] font-medium mt-[10px] stroke-[var(--darkgray)] duration-150 ease-out hover:text-[var(--orange)] hover:underline hover:stroke-[var(--orange)] items-center">                
@@ -136,7 +137,7 @@
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" class="size-5">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75" />
                             </svg>
-                            <span class="text-[14px] md:text-[16px]">birim.sanbartolome@gmail.com</span>
+                            <span class="text-[14px] md:text-[16px] w-fit truncate block">birim.sanbartolome@gmail.com</span>
                         </a>
                         <a href="#" class="flex gap-[10px] text-[16px] items-center text-[var(--darkgray)] font-medium stroke-[var(--darkgray)] duration-150 ease-out hover:text-[var(--orange)] hover:stroke-[var(--orange)] hover:underline">                
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" class="size-6">
@@ -147,6 +148,7 @@
                     </p>
                 </div>
             </div>
+           </div>
         </div>
     </main>
     <!-- Footer Section -->
