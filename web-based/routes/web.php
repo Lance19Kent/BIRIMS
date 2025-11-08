ff6e5c86808c85c9cc3b093f82f4d928d3cf51bb
Sanik0 <sanico.ronan.macugay@gmail.com>
2025-11-08 22:09:08 +0800

    ronan's edit

--- web-based/routes/web.php
+++ web-based/routes/web.php
@@ -33,3 +33,64 @@ Route::get('/terms', function () {
 Route::get('/privacy', function () {
     return view('privacy');
 });
+
+
+Route::get('/home', function () {
+    return view('home');
+});
+
+Route::get('/settings', function () {
+    return view('settings');
+});
+Route::get('/notifications', function () {
+    return view('notifications');
+});
+Route::get('/orders', function () {
+    return view('orders');
+});
+Route::get('/documents', function () {
+    return view('documents');
+});
+Route::get('/blotter', function () {
+    return view('blotter');
+});
+Route::get('/appointment', function () {
+    return view('appointment');
+});
+Route::get('/announcements', function () {
+    return view('announcements');
+});
+Route::get('/verify', function () {
+    return view('verify');
+});
+Route::get('/document', function () {
+    return view('document');
+});
+Route::get('/admin/home', function () {
+    return view('admin.home');
+});
+Route::get('/admin/settings', function () {
+    return view('admin.settings');
+});
+Route::get('/admin/users', function () {
+    return view('admin.users');
+});
+Route::get('/admin/orders', function () {
+    return view('admin.orders');
+});
+Route::get('/admin/announcements', function () {
+    return view('admin.announcements');
+});
+Route::get('/admin/appointment', function () {
+    return view('admin.appointment');
+});
+Route::get('/admin/verifications', function () {
+    return view('admin.verifications');
+});
+Route::get('/admin/blotters', function () {
+    return view('admin.blotters');
+});
+Route::get('/about', function () {
+    return view('about');
+});
+
